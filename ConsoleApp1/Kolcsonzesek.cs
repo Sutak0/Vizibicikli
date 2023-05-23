b4b28f8a7e790519795e0aafe94ddca9f992363e
Sutak0 <sunsamu05@gmail.com>
2023-05-23 12:21:17 +0200

    utolso commit

---
+++ ConsoleApp1/Kolcsonzesek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vizibicikli
+{
+    internal class Kolcsonzes
+    {
+        string nev;
+        char jAzon;
+        int eOra;
+        int ePerc;
+        int vOra;
+        int vPerc;
+
+        public Kolcsonzes(string csvSor)
+        {
+            var mezok = csvSor.Split(';');
+
+            Kolcsonzes uj = new Kolcsonzes(mezok[0], mezok[1][0], int.Parse(mezok[2]), int.Parse(mezok[3]),
+            int.Parse(mezok[4]), int.Parse(mezok[5]));
+        }
+
+        public Kolcsonzes(string nev, char jazon, int eOra, int ePerc, int vOra, int vPerc)
+        {
+            this.nev = nev;
+            this.jAzon = jazon;
+            this.eOra = eOra;
+            this.ePerc = ePerc;
+            this.vOra = vOra;
+            this.vPerc = vPerc;
+        }
+
+        public string Nev { get => nev; }
+        public char Jazon { get => jAzon; }
+        public int EOra { get => eOra; }
+        public int EPerc { get => ePerc; }
+        public int VOra { get => vOra; }
+        public int VPerc { get => vPerc; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace ConVizibicikli
+﻿using System.ComponentModel;
+using vizibicikli;
+using System.Linq;
+
+namespace Vizibicikli
 {
     internal class Program
     {
+        static List<Kolcsonzes> kolcsonzesek = new List<Kolcsonzes>();
         static void Main(string[] args)
         {
-            Console.WriteLine("asd");
+            //Hagyományos technika
+            StreamReader sr = new StreamReader("DataSource\\kolcsonzesek.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                var mezok = sr.ReadLine().Split(';');
+
+                Kolcsonzes uj = new Kolcsonzes(mezok[0],
+                                               mezok[1][0],
+                                               int.Parse(mezok[2]),
+                                               int.Parse(mezok[3]),
+                                               int.Parse(mezok[4]),
+                                               int.Parse(mezok[5]));
+            }
+            sr.Close();
+
+            // LINQ + Foreach
+            kolcsonzesek.Clear();
+
+            foreach (var sor in File.ReadAllLines("DataSource\\kolcsonzesek.txt"))
+            {
+                kolcsonzesek.Add(new Kolcsonzes(sor));
+            }
+
+
+
+            //LINQ
+            List<Kolcsonzes> masikLista
+                = File.ReadAllLines("DataSource\\kolcsonzesek.txt")
+                      .Skip(1)
+                      .Select(x => new Kolcsonzes(x))
+                      .ToList();
+
+            
+            Console.WriteLine($"5. feladat: Napi kölcsönzések száma: {kolcsonzesek.Count}");
+
+
+            Console.Write($"6. feladat: Kérek egy nevet:");
+            String nev = "Kata";
+            Console.WriteLine(nev);
+
+            if (kolcsonzesek.Count(x => x.Nev == "Kata") == 0)
+            {
+                Console.WriteLine("Nem volt ilyen nevű kölcsönző");
+
+            }
+            else {
+                Console.WriteLine($"\t{nev} kölcsönzései: ");
+
+                
+                }
+                 
+            }
         }
     }
-}
